77041b781184a93cad87cf4fbbd7dedf97885df7
asma-abbasi <asmaabbasi7712@gmail.com>
2025-02-04 18:31:55 +0330

    [ADD] add 1-basic-types.ts

---
+++ hello-typescript/src/1-basic-types.ts
@@ -0,0 +1,22 @@
+(function(){
+    const name: string = "asma";
+
+    const age: number = 26;
+
+    const array1: number[] = [1, 2];
+    const array2: Array<string> = ["asma", "abbasi"];
+
+    const isDeveloper: boolean = true;
+
+    const nothing: null = null;
+    const nothing1: undefined = undefined;
+
+    console.log(name);
+    console.log(age);
+    console.log(array1);
+    console.log(array2);
+    console.log(isDeveloper);
+    console.log(nothing);
+    console.log(nothing1);
+
+})
