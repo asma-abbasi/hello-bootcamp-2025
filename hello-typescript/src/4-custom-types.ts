e7f0d42dfd2d4302798b6c3990c47b123d24f722
asma-abbasi <asmaabbasi7712@gmail.com>
2025-02-04 18:43:55 +0330

    [ADD] add 4-custom-types.ts

---
+++ hello-typescript/src/4-custom-types.ts
@@ -0,0 +1,12 @@
+(function (): void {
+
+    type Id = number | string;
+    let id: Id = 12;
+
+    id = "asma98"
+
+    type Status = 'active' | 'inActive'
+    let status: Status = "active"
+    status = "inActive"
+
+})()
