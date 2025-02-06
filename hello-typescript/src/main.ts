93c2139a1c2a237e296e748ed7fa0a4edf97cf71
asma-abbasi <asmaabbasi7712@gmail.com>
2025-02-06 11:28:35 +0330

    [ADD] add count number algorithm

--- hello-typescript/src/main.ts
+++ hello-typescript/src/main.ts
@@ -1,12 +1,20 @@
+import { countNumbers } from "./task-2/count-numbers";
 import { reverseArray } from "./task-2/reverse-array";
 
-function testReverseArray() {
+function testReverseArray(): void {
   const items = [1, 2, 3];
   const reversedItems = reverseArray(items);
   console.log(reversedItems);
 }
 
-function main() {
+function testCounterNumber(): void {
+  const items = [1, "asma", 3, "abbasi"];
+  const count = countNumbers(items);
+  console.log(count);
+}
+
+function main(): void {
   testReverseArray();
+  testCounterNumber();
 }
 main();
